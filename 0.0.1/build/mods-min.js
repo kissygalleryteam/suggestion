5c9ca321a272ff6f67c65f11fbb831112db75aa1
弘树 <tiehang.lth@alibaba-inc.com>
2014-11-14 17:54:43 +0800

    sync daily version 0.0.1

---
+++ 0.0.1/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-11-14 5:52:04 PM*/
+KISSY.config("modules",{"kg/suggestion/0.0.1/index":{requires:["/Users/admin/AliDrive/proj/suggestion/index.css","node","base","io"]}});
---
+++ 0.0.1/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/suggestion/0.0.1/index': { requires: ['/Users/admin/AliDrive/proj/suggestion/index.css', 'node', 'base', 'io']} 
+});
